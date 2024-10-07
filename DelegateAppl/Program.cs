2a266d55e004b5385d2e082651616adddb8a9e36
shanmugabaskar <baskar.shan@gmail.com>
2024-10-07 09:45:53 +0530

    Add project files.

---
+++ DelegateAppl/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DelegateAppl;
+
+//create delegate instances
+NumberChanger nc1 = new NumberChanger(TestDelegate.AddNum);
+NumberChanger nc2 = new NumberChanger(TestDelegate.MultiNum);
+
+//calling the methods using the delegate objects
+nc1(25);
+Console.WriteLine("Value of Num: {0}", TestDelegate.getNum());
+nc2(5);
+Console.WriteLine("Value of Num: {0}", TestDelegate.getNum());
+
+//create delegate instances
+NumberChanger nc;
+NumberChanger nc3 = new NumberChanger(TestDelegate.AddNum);
+NumberChanger nc4 = new NumberChanger(TestDelegate.MultiNum);
+
+nc = nc3;
+nc += nc4;
+
+//calling multicast
+nc(5);
+Console.WriteLine("Value of Num: {0}", TestDelegate.getNum());
+Console.ReadKey();
---
+++ DelegateAppl/TestDelegate.cs
@@ -0,0 +1,24 @@
+﻿delegate int NumberChanger(int n);
+
+namespace DelegateAppl
+{
+    internal class TestDelegate
+    {
+        static int num = 10;
+
+        public static int AddNum(int p)
+        {
+            num += p;
+            return num;
+        }
+        public static int MultiNum(int q)
+        {
+            num *= q;
+            return num;
+        }
+        public static int getNum()
+        {
+            return num;
+        }
+    }
+}
